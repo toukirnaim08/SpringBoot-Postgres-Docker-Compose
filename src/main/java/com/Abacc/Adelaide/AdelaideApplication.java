81d0d43f983d150f75854bf9e29527bb153d3b89
toukirnaim08 <toukir08@gmail.com>
2021-05-04 03:05:39 +0930

    Adedd initial files

---
+++ src/main/java/com/Abacc/Adelaide/AdelaideApplication.java
@@ -0,0 +1,13 @@
+package com.Abacc.Adelaide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdelaideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdelaideApplication.class, args);
+	}
+
+}
